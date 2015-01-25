7d51580d4f8d273c574e3df1e88d4d0d34e9557b
flyfj <fj333xx@gmail.com>
2015-01-25 10:38:58 -0500

    filter empty models in modelnet

--- DataProcessor/DataProcessor/modelnet_picker.py
+++ DataProcessor/DataProcessor/modelnet_picker.py
@@ -22,16 +22,27 @@ sel_cate_ids = random.sample(range(len(cates)), sel_cate_num)
 print sel_cate_ids
 
 sel_obj_num = 10
-with open('model_list.txt', 'w') as file:
+with open('model_list_nonempty.txt', 'w') as file:
     # select objects and save to file
     for cate_id in sel_cate_ids:
         cur_cate_name = cates[cate_id]
         cur_dir = model_root + cur_cate_name + '/'
         cur_objs = os.listdir(cur_dir)
-        sel_obj_ids = random.sample(range(len(cur_objs)), min(len(cur_objs), sel_obj_num))
-        for obj_id in sel_obj_ids:
-            sel_obj_fn = cur_cate_name + '/' + cur_objs[obj_id] + '/' + cur_objs[obj_id]
-            file.write(sel_obj_fn + '\n')
+        random.shuffle(cur_objs)
+        valid_num = 0
+        for idx in range(len(cur_objs)):
+            # check if model file exists and is non-empty
+            sel_obj_fn = cur_cate_name + '/' + cur_objs[idx] + '/' + cur_objs[idx]
+            model_fn = model_root + sel_obj_fn + '.off'
+            try:
+                if os.path.getsize(model_fn) > 0:
+                    file.write(sel_obj_fn + '\n')
+                    valid_num += 1
+                    if valid_num == sel_obj_num:
+                        break
+            except Exception:
+                pass
+    
         print 'finished ' + cur_cate_name
 
 print 'done'
